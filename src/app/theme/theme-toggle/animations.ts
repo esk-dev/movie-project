0e3a90af945a09c94a128adf30e808ce32a307b9
Egor <eesk98@gmail.com>
2022-12-05 10:32:10 +0000

    fix[delete file]

--- src/app/theme/theme-toggle/animations.ts
+++
@@ -1,9 +0,0 @@
-import { animation, style, animate, trigger, transition, useAnimation } from '@angular/animations';
-
-export const fadeIn = animation([
-  style({
-    opacity: 0,
-    transform: translateY(10px);
-  }),
-  animate('100ms')
-]);
